64ca6c2783504a4298d7406bd5612a16ea16f258
saqibhussain44 <saqibhussain990@gmail.com>
2020-03-14 00:08:40 +0500

    render sample pk map from highmaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MapChart } from 'angular-highcharts';
+import { HttpClient } from '@angular/common/http';
+
+declare var require: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pak-covid19-client';
+  map: MapChart = null;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    const pkMapData = require('@highcharts/map-collection/countries/pk/pk-all.geo.json');
+    this.draw(pkMapData);
+  }
+
+  draw(pkMapData) {
+    console.log('pkMapData', pkMapData);
+    const data: any = [
+      ['pk-sd', 0],
+      ['pk-ba', 1],
+      ['pk-jk', 2],
+      ['pk-na', 3],
+      ['pk-nw', 4],
+      ['pk-ta', 5],
+      ['pk-is', 6],
+      ['pk-pb', 7]
+    ];
+    const series: any = [
+      {
+        data,
+        name: 'Random data',
+        states: {
+          hover: {
+            color: '#BADA55'
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          format: '{point.name}'
+        }
+      }
+    ];
+    this.map = new MapChart({
+      chart: {
+        map: pkMapData,
+      },
+      title: {
+        text: 'Highmaps basic demo'
+      },
+      subtitle: {
+        text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/pk/pk-all.js">Pakistan</a>'
+      },
+      mapNavigation: {
+        enabled: true,
+        buttonOptions: {
+          verticalAlign: 'bottom'
+        }
+      },
+      colorAxis: {
+        min: 0
+      },
+      series
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as more from 'highcharts/highcharts-more.src';
+import * as exporting from 'highcharts/modules/exporting.src';
+import * as highmaps from 'highcharts/modules/map.src';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ChartModule
   ],
-  providers: [],
+  providers: [
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [ more, exporting, highmaps ] } // add as factory to your providers
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
